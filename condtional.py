86a3b7ec53291b14116e0a9b38373cab784ecb6c
dheeraj <babusahabdheeraj@gmail.com>
2026-07-21 01:50:45 +0530

    add

--- condtional.py
+++ condtional.py
@@ -40,12 +40,14 @@ else:
 #     print(f"{num} is not divisible by 3 and 7")
 
 #finding the greatest number among three numbers
-a=int(input("enter the first number:"))
-b=int(input("enter the second number:"))
-c=int(input("enter the third number:"))
-if a>b and a>c:
-     print(f"{a} is the greatest number")
-elif b>a and b>c:
-    print(f"{b} is the greatest number")
-else:
-    print(f"{c} is the greatest number")
+# a=int(input("enter the first number:"))
+# b=int(input("enter the second number:"))
+# c=int(input("enter the third number:"))
+# if a>b and a>c:
+#      print(f"{a} is the greatest number")
+# elif b>a and b>c:
+#     print(f"{b} is the greatest number")
+# else:
+#     print(f"{c} is the greatest number")
+
+
