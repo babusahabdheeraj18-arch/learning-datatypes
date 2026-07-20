ab257163ee27b5d3704d7efdc68633d05e6316b6
dheeraj <babusahabdheeraj@gmail.com>
2026-07-20 11:44:35 +0530

    conditional statement

---
+++ condtional.py
@@ -0,0 +1,21 @@
+'''a=int(input("enter the number:"))
+if a<0:
+    print("a is negative")
+else:
+    print("a is positive")'''
+
+#2 looking for greater number 
+# a=int(input("enter the first number:"))
+# b=int(input("enter the second number:"))
+# if a<b:
+#     print("a is less then b")
+# else:
+#     print("a is greater then b")
+
+#checking whetehr student is pass or fail
+marks=int(input("enter the marks:"))
+student=input("enter the student name:")
+if marks>=33:
+    print(f"{student} is pass")
+else:
+    print(f"{student} is fail")
