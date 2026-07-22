69537598e95aa8ea03617db143045826fd885809
dheeraj <babusahabdheeraj@gmail.com>
2026-07-23 02:00:03 +0530

    for loop

---
+++ forloop.py
@@ -0,0 +1,22 @@
+#count the number of positive number in the list 
+# num=[1,-2,3,-4,5,-6,7,-8,9,10]
+# n = len(num)
+# count=0
+# for i in num:
+#     if i<0:
+#         count+=1
+# print("Number of positive numbers:", n-count)
+
+#calculate the sum of even no upto a given numbers 
+# sum=0
+# for i in range(1,21):
+#     if i%2==0:
+#         sum+=i
+# print("Sum of even numbers:", sum)
+
+# #calculate the sum of odd numbers upto a given numbers
+# sum=0
+# for i in range(1,21):
+#     if i%2!=0:
+#         sum+=i
+# print("Sum of odd numbers:", sum)
