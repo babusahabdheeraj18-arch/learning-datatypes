65b210de439454e31d60e24100ce1c458847965e
dheeraj <babusahabdheeraj@gmail.com>
2026-07-23 23:55:25 +0530

    while loop pratice

---
+++ whileloop.py
@@ -0,0 +1,36 @@
+#print all the even between 1 to 20 using while loop
+# i = 1
+# while i<=20:
+#     if i%2==0:
+#         print(i)
+#     i=i+1
+
+# #print all the odd between 1 to 20 using while loop
+# i = 1
+# while i<=20:
+#     if i%2!=0:
+#         print(i)
+#     i=i+1
+
+#print multiplication table of a given number using while loop
+# num=int(input("Enter a number: "))
+# i=1
+# while i<=10:
+#     print(num,"*",i,"=",num*i)
+#     i+=1
+
+#Keep asking the user to enter a number until they enter 0
+# num=int(input("Enter a number: "))
+# while num!=0:
+#     print("You entered:", num)
+#     num=int(input("Enter a number (0 to exit): "))
+
+#Ask the user to enter a password until they enter the correct password.
+# correct_password="python123"
+# password=input("Enter the password: ")
+# while password!=correct_password:
+#     print("Incorrect password. Please try again.")
+#     password=input("Enter the password: ")
+# print("Access granted.")
+
+#Keep taking numbers from the user and find their sum. Stop when the user enters -1.
